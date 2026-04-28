4ac904b1f0133a86ea66da9eeb6dc8a6f3ef9299
Erik Beresenkow <erik.beresenkow@proton.me>
2026-04-28 19:21:06 +0200

    feat: parse and pass only usefull nowcast weather data to the ai

--- backend/src/weatherForecast.py
+++ backend/src/weatherForecast.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import math
 from datetime import datetime
 import os
 from dotenv import load_dotenv
@@ -56,10 +57,24 @@ def get_current_weather():
     if response.status_code == 200:
         data = response.json()
         place_name = data['name']
-        current_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     
-        print(f"Wetter um {current_time} in {place_name}: {data}")
-        return response.json
+        curr_temp = convert_celvin_to_celcius(data['main']['temp'])
+        fells_like = convert_celvin_to_celcius(data['main']['feels_like'])
+
+        weather_cond_main = data['weather'][0]['main']
+        weather_cond_description = data['weather'][0]['description']
+
+        wind_speed = data['wind']['speed']
+        wind_direction = deg_to_compass(data['wind']['deg'])
+
+        weather_nowcaset_string = (
+            f"Das Wetter in {place_name} ist aktuell bei {round_half_up(curr_temp)} °C; Gefühlt sind {round_half_up(fells_like)} °C. "
+            f"Bei hauptsächlich {weather_cond_main} und {weather_cond_description} Wetter. "
+            f"Mit Windgeschwindigkeiten von {wind_speed} km/h aus {wind_direction} kommend."
+        )
+
+        print(weather_nowcaset_string)
+        return weather_nowcaset_string
 
 def get_weather_forecast():
     cords = get_cords()
@@ -76,3 +91,17 @@ def get_weather_forecast():
     
         print(f"Wetter um {current_time} in {place_name}: {data}")
         return response.json
+
+def convert_celvin_to_celcius(deg_kelvin):
+    return deg_kelvin - 273.15
+
+def deg_to_compass(deg):
+    directions = ['Norden', 'Nord-Nord-Ost', 'Nord-Ost', 'Ost-Nord-Ost', 
+                  'Osten', 'Ost-Süd-Ost', 'Süd-Ost', 'Süd-Süd-Ost', 
+                  'Süden', 'Süd-Süd-West', 'Süd-West', 'West-Süd-West', 
+                  'Westen', 'West-Nord-West', 'Nord-West', 'Nord-Nord-West']
+    index = round(deg / 22.5) % 16
+    return directions[index]
+
+def round_half_up(n):
+    return math.floor(n * 10 + 0.5) / 10
