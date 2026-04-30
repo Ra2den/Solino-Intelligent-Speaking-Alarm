a8ef9398de22e0d9d7f6a10d82ec6f0499bcb705
Luca Rosato <99467245+shirouuuu@users.noreply.github.com>
2026-04-30 20:52:34 +0200

    feat: responsive face and guard expression

--- frontend/src/components/Agent.tsx
+++ frontend/src/components/Agent.tsx
@@ -1,6 +1,7 @@
 import solinoBase from "../assets/agent/solino_base.svg";
 import solinoRing from "../assets/agent/solino_ring.svg";
 import expressionDefault from "../assets/agent/expression_default.svg";
+import expressionRaisedBrows from "../assets/agent/expression_guard.svg";
 import { useWeatherNowcast } from "../hooks/weather/useWeatherNowcast";
 import { type WeatherCondition } from "../models/weather/weather-nowcast.model";
 
@@ -21,11 +22,6 @@ export function Agent() {
     <>
       <div className="relative bg-blue-100 w-full h-full p-6">
         <div className="font-medium text-5xl">{weatherData.temperature}°C</div>
-        <img
-          className="z-1 absolute w-30 top-70 left-1/2 -translate-x-1/2 -translate-y-1/2"
-          src={expressionDefault}
-          alt="Happy Expression of Solino"
-        />
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <img
             className="w-150 max-w-none object-contain animate-[spin_15s_linear_infinite]"
@@ -33,14 +29,37 @@ export function Agent() {
             alt="Solino Ring"
           />
         </div>
-        <img
-          className="absolute top-1/2 w-100 left-1/2 -translate-x-1/2 -translate-y-1/2"
-          src={solinoBase}
-          alt="Solino Base"
-        />
+        <div className="absolute top-1/2 left-1/2 w-100 -translate-x-1/2 -translate-y-1/2">
+          <img
+            className="w-full object-contain"
+            src={solinoBase}
+            alt="Solino Base"
+          />
+          {getExpression(false)}
+        </div>
       </div>
     </>
   );
+
+  function getExpression(isGuard: boolean) {
+    if (isGuard) {
+      return (
+        <img
+          className="absolute top-[25%] left-1/2 z-1 w-[35%] -translate-x-1/2"
+          src={expressionRaisedBrows}
+          alt="Expression of Solino with raised Eyebrows"
+        />
+      );
+    } else {
+      return (
+        <img
+          className="absolute top-[25%] left-1/2 z-1 w-[30%] -translate-x-1/2"
+          src={expressionDefault}
+          alt="Happy Expression of Solino"
+        />
+      );
+    }
+  }
 
   function determineWeatherDisplay(weatherCondition: WeatherCondition) {
     switch (weatherCondition) {
