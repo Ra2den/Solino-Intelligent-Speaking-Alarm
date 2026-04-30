749630aafef64b1e8bec71ca8e75c5152317c2a6
beresenkow_notebook <erik.beresenkow2@gmail.com>
2026-04-30 20:52:16 +0200

    feat: improve stability of weather api endpoints by raising HTTPExceptions, when openwaetherapi fails

--- backend/src/api.py
+++ backend/src/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from typing import Optional, List
 import db
@@ -65,19 +65,35 @@ def delete_alarm(alarm_id: int):
 # Fetch current weather as a WeatherNowcast object
 @app.get("/weather/nowcast", response_model=WeatherNowcast)
 def get_weather_nowcast():
-    return weatherForecast.get_current_weather_for_api()
+    result = weatherForecast.get_current_weather_for_api()
+    if result == None:
+        raise HTTPException(status_code=502, detail="Weather service error")
+
+    return result
 
 # Fetch weather forecast for the next 5 days in 3 hour intervalls
 @app.get("/weather/forecast", response_model=WeatherForecast)
 def get_weather_forecast():
-    return weatherForecast.get_weather_forecast_for_api()
+    result = weatherForecast.get_weather_forecast_for_api()
+    if result == None:
+        raise HTTPException(status_code=502, detail="Weather service error")
+
+    return result
 
 # Fetch time of todays sunrise
 @app.get("/weather/sunrise", response_model=Sunrise)
 def get_sunrise_time():
-    return weatherForecast.get_sunrise_time()
+    result = weatherForecast.get_sunrise_time()
+    if result == None:
+        raise HTTPException(status_code=502, detail="Weather service error")
+
+    return result
 
 # Fetch time of todays sunset
 @app.get("/weather/sunset", response_model=Sunset)
 def get_sunset_time():
-    return weatherForecast.get_sunset_time()
+    result = weatherForecast.get_sunset_time()
+    if result == None:
+        raise HTTPException(status_code=502, detail="Weather service error")
+
+    return result
